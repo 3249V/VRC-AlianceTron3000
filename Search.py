ebb34901d5214b7b45b942c635a43b9b541e0d81
3249V <55768669+3249V@users.noreply.github.com>
2020-01-09 08:01:58 -0800

    Update Search.py

--- Search.py
+++ Search.py
@@ -21,45 +21,54 @@ entryText = StringVar()
 def rofl(frame2):
     bigFrame.destroy()
     frame2.destroy()
-    what = Label(root, text="Not Finished Yet")
-    what.pack(pady=10)
+    what = Label(root, text="Search Mode:")
+    what.pack(pady=5)
+
+    mode = Radiobutton(root, text="Search Team stats", variable=modeVar, value=1)
+    mode.pack()
+    mode2 = Radiobutton(root, text="Alliance Picker", variable=modeVar, value=2)
+    mode2.pack()
+
+    checkLabels = Label(root, text = "Result Options:", font = ('helvetica', 9))
+    checkLabels.pack(pady = 5)
 
     RankCheckbox = Checkbutton(root, text="Show last tournament rank", variable=RankCheck)
     RankCheckbox.pack(pady=2)
 
-
     WPCheckbox = Checkbutton(root, text="Show Win Points", variable=WPCheck)
     WPCheckbox.pack(pady=2)
-
 
     checkbox = Checkbutton(root, text="Show Auton Points", variable=AutonCheck)
     checkbox.pack(padx=10, pady=2)
 
     if AutonCheck == 1:
         checkbox.select()
-    idk = Button(root, text="Back", command=lambda: WhyEven(idk, what, checkbox, RankCheckbox, WPCheckbox))
+    idk = Button(root, text="Back", command=lambda: WhyEven(idk, what, checkbox, RankCheckbox, WPCheckbox, mode, mode2))
     idk.pack(pady=10)
+
 
 def searchDestroy():
     print("idk")
+
+
 class WhyEven():
-    def __init__(self, t1, t2, t3, t4, t5):
+    def __init__(self, t1, t2, t3, t4, t5, t6, t7):
         t1.destroy()
         t2.destroy()
         t3.destroy()
         t4.destroy()
         t5.destroy()
+        t6.destroy()
+        t7.destroy()
         bigFrame2 = Frame(root)
         bigFrame2.pack(ipadx=100)
         print("back")
-        MainSearch(bigFrame2)
-
-
+        MainSearch(bigFrame2, modeVar.get())
 
 
 class MainSearch():
-    searchFrame = Frame(bigFrame)
-    searchFrame.pack(side=TOP)
+    #searchFrame = Frame(bigFrame)
+    #searchFrame.pack(side=TOP)
     def mode_switch(self):
 
         # searchsave = self.searchBar.get()
@@ -73,8 +82,8 @@ class MainSearch():
         print("Entry: "+ str(entryText))
         searchBar.pack(pady=2)
 
+    def __init__(self, master, mode):
 
-    def __init__(self, master):
         testFrame = Frame(master)
         test = Label(testFrame)
         test.pack()
@@ -84,34 +93,32 @@ class MainSearch():
 
         settingsFrame = Frame(master)
         settingsFrame.pack(fill=X, side=TOP)
-        #self.searchFrame = Frame(master)
-        #self.searchFrame.pack(side=TOP)
+        if mode == 1:
+            self.searchFrame = Frame(master)
+            self.searchFrame.pack(side=TOP)
 
         bottomFrame = Frame(master)
         bottomFrame.pack(side=TOP)
 
         self.settingsButton = Button(settingsFrame, image=photo, width=30, command=lambda: rofl(master))
         self.settingsButton.pack(side=LEFT, padx=5, pady=5)
-
-        self.Title = Label(self.searchFrame, text="Enter the team you want to look up below:", textvariable = entryText)
-        self.Title.pack()
-
-        self.searchBar = Entry(self.searchFrame, textvariable = entryText)
-        print("Entry: "+ str(entryText))
-        self.searchBar.pack(pady=2)
-
-        self.button = Button(self.searchFrame, text="Search", command=self.get_data)
-        self.button.pack()
+        if mode == 1:
+            self.Title = Label(self.searchFrame, text="Enter the team you want to look up below:", textvariable = entryText)
+            self.Title.pack()
+        if mode ==1:
+            self.searchBar = Entry(self.searchFrame, textvariable = entryText)
+            #print("Entry: "+ str(entryText))
+            self.searchBar.pack(pady=2)
+        if mode == 1:
+            self.button = Button(self.searchFrame, text="Search", command=self.get_data)
+            self.button.pack()
 
         self.resultTitle = Label(bottomFrame, text="Results:")
         self.resultTitle.pack(pady=5)
 
         self.StatList = Listbox(bottomFrame, width=40)
         self.StatList.pack(ipadx=5)
-        self.mode = Radiobutton(master, text="Search Team stats", variable=modeVar, value=1, command = lambda : self.mode_switch2())
-        self.mode.pack()
-        self.mode = Radiobutton(master, text="Alliance Picker", variable=modeVar, value=2, command = self.mode_switch)
-        self.mode.pack()
+
 
         self.master.bind('<Return>', self.get_data)
 
@@ -162,6 +169,6 @@ testList = ["a", "b"]
 
 #entryText = StringVar()
 
-b = MainSearch(bigFrame)
+b = MainSearch(bigFrame, 1)
 
 root.mainloop()
